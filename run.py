d2de81e76934d02d664ea482e01aabe312c387ce
TCaldato <tcaldato@gmail.com>
2023-08-28 07:19:57 +0100

    Added class Battleship, changed import random and added comment

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+#Built-in module to make random numbers.
+
+class Board:
+    """
+    Main Board class that is reponsible for managing the game board
+    """
+    def __init__(self, size):
+        self.size = size
+        self.grid = [['0'] * size for _ in range(size)]
+
+    def display(self):
+        for row in self.grid:
+            print(" ".join(row))
+
+    def is_valid(self, row, col):
+        return 0 <= row < self.size and 0 <= col < self.size
+    
+class Battelship:
+    """
+    Class responsible for managing the battleship's location and checking 
+    guesses
+    """
+    def __init__(self, board_size):
+        self.row = random.randint(0, board_size - 1)
+        
