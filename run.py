e6a277733ae550b41b867d3c0d0f23129b7806a2
TCaldato <tcaldato@gmail.com>
2023-08-31 06:41:26 +0100

    Add computer's board with its attempts, and welcome  phrase when game starts

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-import random
-#Built-in module to make random numbers.
+import random #Built-in module to make random numbers.
 
 class Board:
     """
@@ -16,7 +15,7 @@ class Board:
     def is_valid(self, row, col):
         return 0 <= row < self.size and 0 <= col < self.size
 
-    
+
 class Battleship:
     """
     Class responsible for managing the battleship's location and checking 
@@ -36,42 +35,90 @@ class Battleship:
             return "miss"
 
 
+class ComputerBoard(Board):
+    """
+    Class for managing the computer's board
+    """
+    def __init__(self, size, battleship=None):
+        super().__init__(size)
+        if battleship:
+            self.battleship = battleship
+            self.grid[self.battleship.row][self.battleship.col] = 'O'
+
 class Game:
     """
     Class responsible for managing the game flow and player interaction
     """
     def __init__(self, board_size):
-        self.board = Board(board_size)
-        self.battleship = Battleship(board_size)
-        self.attempts = 1
+        self.user_board = Board(board_size)
+        self.user_battleship = Battleship(board_size)
+        self.user_attempts = 1
         
-    def play(self):
-        while True:
-            print("\nYou have 5 Guesses: Attempt", self.attempts)
-            self.board.display()
+        # Create a battleship object for the computer
+        computer_battleship = Battleship(board_size)
+        self.computer_board = ComputerBoard(board_size, computer_battleship)
+        self.computer_battleship = computer_battleship
+        
 
+    def unique_random_guess(self):
+        while True:
+            guess_row = random.randint(0, self.computer_board.size - 1)
+            guess_col = random.randint(0, self.computer_board.size - 1)
+            if self.computer_board.grid[guess_row][guess_col] == '*': 
+                return guess_row, guess_col
+            
+    
+    def play(self):
+        """
+        Function responsible for interaction with user and computer
+        """
+        while True:
+            print("\nYour turn! You have 5 Guesses: Attempt", self.user_attempts)
+            self.user_board.display()
             guess_row = int(input("Guess a Row: "))
             guess_col = int(input("Guess a Column: "))
+            self.user_attempts += 1        
             
-            if not self.board.is_valid(guess_row, guess_col):
+            if not self.user_board.is_valid(guess_row, guess_col):
                 print("Oops, Values must be between 0 and 4!")
             else:
-                self.attempts += 1
-                result = self.battleship.check_guess(guess_row, guess_col)
+                result = self.user_battleship.check_guess(guess_row, guess_col)
                 
                 if result == "hit":
                     print("Congratulations! You sunk my battleship!")
                     break
                 else:
                     print("You missed my battleship!")
-                    self.board.grid[guess_row][guess_col] = 'X'
+                    self.user_board.grid[guess_row][guess_col] = 'X'
                 
-                if self.attempts >= 6:
+                if self.user_attempts >= 6:
                     print("Game Over! You've used all your attempts.")
-                    print("The battleship was located at row", self.battleship.row, "and column", self.battleship.col)
+                    print("The battleship was located at row", self.user_battleship.row, "and column", self.user_battleship.col)
                     break
+
+
+                print("\nComputer's turn")
+                self.computer_board.display()
+                guess_row, guess_col = self.unique_random_guess()
+                result = self.computer_battleship.check_guess(guess_row, guess_col)
+                # Process computer's guess and update computer's board
+                
+                if result == "hit":
+                    print("Oh no! The computer hit your battleship!")
+                    break
+                else:
+                    print("Phew! The computer missed your battleship!")
+                    self.computer_board.grid[guess_row][guess_col] = 'O'                    
+
                     
 def main():
+    """
+    Main function to start the game
+    """
+    print("\nWelcome to Battleship Game")
+    print("In this game you have 5 attempts to try sink computer's ship")
+    print("Do you think you can do it???")
+    print("Let's play!!!")
     board_size = 5
     game = Game(board_size)
     game.play()
